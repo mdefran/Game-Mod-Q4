0151528db833e0d88a14ab39780381a84c49a8ce
mdefran <matthew.defranceschi@gmail.com>
2023-09-14 00:19:53 -0400

    successfully added blaster changes

--- game/weapon/WeaponBlaster.cpp
+++ game/weapon/WeaponBlaster.cpp
@@ -427,11 +427,11 @@ stateResult_t rvWeaponBlaster::State_Fire ( const stateParms_t& parms ) {
 
 	
 			if ( gameLocal.time - fireHeldTime > chargeTime ) {	
-				Attack ( true, 1000, 100, 0, 0.1f );
+				Attack ( true, 1000, 1000, 0, 0.1f );
 				PlayEffect ( "fx_chargedflash", barrelJointView, false );
-				PlayAnim( ANIMCHANNEL_ALL, "chargedfire", parms.blendFrames );
+				PlayAnim( ANIMCHANNEL_ALL, "fire", parms.blendFrames );
 			} else {
-				Attack ( false, 1, spread, 0, 1.0f );
+				Attack ( false, 1, spread, 0, 0.1f);
 				PlayEffect ( "fx_normalflash", barrelJointView, false );
 				PlayAnim( ANIMCHANNEL_ALL, "fire", parms.blendFrames );
 			}
